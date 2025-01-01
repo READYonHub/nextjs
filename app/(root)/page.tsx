b106faf9c795fb06d78cf8b827ca97fd1c5bb1a6
READYonHub <readyemail3@gmail.com>
2025-01-01 22:50:22 +0100

    05  All Startups & Startup Card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,21 @@
-import Image from "next/image";
 import SerachForm from "../../components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({ searchParams }: {
   searchParams: Promise<{ query?: string }>
 }) {
   const query = (await searchParams).query;
+
+  const posts = [{
+    title: "Fejlesztő",
+    authorr: { _id: 1 },
+    category: "Normális állás",
+    description: 'Itt vannak juttatások, megbecsülés és versenyképes fizetés is!',
+    views: 55,
+    _createdAt: new Date(),
+    image: "https://as2.ftcdn.net/v2/jpg/02/10/96/95/1000_F_210969565_cIHkcrIzRpWNZzq8eaQnYotG4pkHh0P9.jpg"
+  },]
+    ;
 
   return (
     <>
@@ -18,6 +29,22 @@ export default async function Home({ searchParams }: {
         <SerachForm query={query} />
 
       </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Keresett eredmény: "${query}"` : "Minden más"}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
+      </section>
+
     </>
   );
 }
